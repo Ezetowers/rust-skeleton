25bfd5ad43bb6a1c74d5631984f756806e011b1a
Ezequiel Torres Feyuk <ezequiel.torresfeyuk@gmail.com>
2021-12-18 23:59:56 -0300

    Added run function before library split

--- src/bin/minigrep.rs
+++ src/bin/minigrep.rs
@@ -1,6 +1,7 @@
 use std::env;
 use std::fs;
 use std::process;
+use std::error::Error;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -27,10 +28,26 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
 
-    let contents = fs::read_to_string(config.filename)
-        .expect("Something went wrong reading the file");
+    // Since the run function returns () in the Ok value of the Result,
+    // there is nothing to unwrap and we can check the error directly
+    if let Err(e) = run(config) {
+        println!("Application error: {}", e);
+        process::exit(1);
+    }
+}
 
+// This function will return a type that implements the Error trait, 
+// but we don’t have to specify what particular type the return value will be
+// That's the meaning of dyn, the error will be known in deploy time (polymorphism)
+// I don't know what the Box thing does yet U_U
+//
+// Regarding the return value of the Ok part of the result, we are returning () which
+// is the void value of Rust. It is the value that would be used if no result would
+// be used here. This is the correct way in Rust to return nothing
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.filename)?;
     println!("With text:\n{}", contents);
+    Ok(())
 }
 
 struct Config {
