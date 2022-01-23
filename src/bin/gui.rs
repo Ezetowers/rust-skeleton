3d9e68bbd69261b799173ff2b900116a76072875
Ezequiel Torres Feyuk <ezequiel.torresfeyuk@gmail.com>
2022-01-23 19:45:43 -0300

    More changes

--- src/bin/gui.rs
+++ src/bin/gui.rs
@@ -1,29 +1,5 @@
 use rust_skeleton::{Screen, SelectBox, Button};
 
-/*------------------------------------------------------------------------*/
-
-// This is the equivalent of defining a vector of elements
-// of the same type in C++. Since this code is solved at
-// compile time, all the elements of the components vector
-// must be of the same type. The not commented definition of the Screen struct
-// let the user define diferent objects in the vector that are compliant with
-// the trait since the object type is determined at runtime. As always, 
-// figure out things at runtime is slower (v-table)
-// pub struct Screen<T: Draw> {
-//     pub components: Vec<T>,
-// }
-
-// impl<T> Screen<T>
-// where
-//     T: Draw,
-// {
-//     pub fn run(&self) {
-//         for component in self.components.iter() {
-//             component.draw();
-//         }
-//     }
-// }
-
 fn main() {
     let screen = Screen {
         components: vec![
--- src/lib.rs
+++ src/lib.rs
@@ -131,6 +131,28 @@ impl Screen {
     }
 }
 
+// This is the equivalent of defining a vector of elements
+// of the same type in C++. Since this code is solved at
+// compile time, all the elements of the components vector
+// must be of the same type. The not commented definition of the Screen struct
+// let the user define diferent objects in the vector that are compliant with
+// the trait since the object type is determined at runtime. As always, 
+// figure out things at runtime is slower (v-table)
+// pub struct Screen<T: Draw> {
+//     pub components: Vec<T>,
+// }
+
+// impl<T> Screen<T>
+// where
+//     T: Draw,
+// {
+//     pub fn run(&self) {
+//         for component in self.components.iter() {
+//             component.draw();
+//         }
+//     }
+// }
+
 pub struct Button {
     pub width: u32,
     pub height: u32,
